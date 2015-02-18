54cc19c0d32eac02943788422fbd57a260bcf1b1
Ariel Keselman <skariel@gmail.com>
2015-02-18 09:46:10 +0200

    changes

--- nbody.jl
+++ nbody.jl
@@ -3,6 +3,8 @@ module nbody
 using OctTrees
 import OctTrees: modify, stop_cond, getx, gety, getz
 
+using GeometricalPredicates
+
 include("types.jl")
 include("simulation.jl")
 include("forces.jl")
--- realizations.jl
+++ realizations.jl
@@ -1,5 +1,5 @@
-function worldnormal(n::Int64; smth=0.01, opening_excluded_frac=0.6, opening_alpha=0.7, dtfrac=0.035)
-    particles = [Particle(randn(), randn(), randn(), 1./n) for i in 1:n]
+function World(particles::Array{Particle, 1}, smth, opening_excluded_frac, opening_alpha, dtfrac)
+    n = length(particles)
     World(
         OctTree(Particle; n=trunc(Integer,4.1*n)),
         particles,
@@ -17,27 +17,26 @@ function worldnormal(n::Int64; smth=0.01, opening_excluded_frac=0.6, opening_alp
     )
 end
 
-function worldspherical(n::Int64; smth=0.01, opening_excluded_frac=0.6, opening_alpha=0.7, dtfrac=0.035)
+particlesnormal(n::Int64) = hilbertsort!([Particle(randn(), randn(), randn(), 1./n) for i in 1:n])
+
+function particlesspherical(n::Int64)
     particles = Particle[]
     while length(particles) < n
-        tx = rand()*2.0-1.0
-        ty = rand()*2.0-1.0
-        tz = rand()*2.0-1.0
-        tx*tx+ty*ty+tz*tz < 1.0 && push!(particles, Particle(tx,ty,tz, 1./n))
+        x = 2.0*rand()-1.0
+        y = 2.0*rand()-1.0
+        z = 2.0*rand()-1.0
+        if x*x+y*y+z*z < 1
+            push!(particles, Particle(x,y,z,1.0/n))
+        end
     end
-    World(
-        OctTree(Particle; n=trunc(Integer,4.1*n)),
-        particles,
-        zeros(n),
-        zeros(n),
-        zeros(n),
-        zeros(n),
-        zeros(n),
-        zeros(n),
-        n,
-        opening_alpha^2,
-        opening_excluded_frac^2,
-        smth*smth,
-        dtfrac
-    )
+    hilbertsort!(particles)
 end
+
+world(particles::Array{Particle, 1}; smth=0.01, opening_excluded_frac=0.6, opening_alpha=0.7, dtfrac=0.035) =
+    World(particles, smth, opening_excluded_frac, opening_alpha, dtfrac)
+
+worldnormal(n::Int64; smth=0.01, opening_excluded_frac=0.6, opening_alpha=0.7, dtfrac=0.035) =
+    World(particlesnormal(n), smth, opening_excluded_frac, opening_alpha, dtfrac)
+
+worldspherical(n::Int64; smth=0.01, opening_excluded_frac=0.6, opening_alpha=0.7, dtfrac=0.035) =
+    World(particlesspherical(n), smth, opening_excluded_frac, opening_alpha, dtfrac)
